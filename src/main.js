fcdf6e5104dd82c162effabe9e61ca7ec7b68f5a
Branden Orefice <Bran.Orefice@gmail.com>
2024-08-28 20:30:33 -0400

    Added a featured section, images, and edited features

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
+import '../styles/components/featured.css';
 import '../styles/components/projects.css';
 import '../styles/components/contact.css';
 import '../styles/components/footer.css';
@@ -11,6 +12,8 @@ import '../styles/utilities.css';
 
 import mobileNav from './utilities/mobile-ham';
 import darkMode from './utilities/dark-mode';
+import lazyLoading from './utilities/lazy-loading';
 
 mobileNav();
 darkMode();
+lazyLoading();
